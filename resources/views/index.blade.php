7c63c1ffa0b10ad7f11a61ba8a7b0193697bce5f
asmi046 <asmi046@gmail.com>
2023-06-21 19:17:43 +0300

    Старт

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+
+@extends('layouts.all')
+
+
+@section('title', "Телефонный справочник мансурово")
+@section('description', "Телефонный справочник мансурово")
+
+@section('content')
+
+<h1 class="text-xl text-red-900">ТС</h1>
+
+@endsection
