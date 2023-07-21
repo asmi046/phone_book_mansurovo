fff13fa548051bcfacaa0f6dd856b936278c2936
asmi046 <asmi046@gmail.com>
2023-07-21 16:37:34 +0300

    Запрос

--- app/Http/Controllers/AbonentController.php
+++ app/Http/Controllers/AbonentController.php
@@ -9,7 +9,8 @@ use App\Models\Abonent;
 class AbonentController extends Controller
 {
     public function get_abonent(Request $request) {
-        $result = Abonent::all();
-        return $result;
+        $search = empty($request->input('search'))?"%":"%".$request->input('search')."%";
+        $result = Abonent::where('name', "LIKE", $search)->get();
+        return ['search' =>$search, 'abonents' => $result];
     }
 }
